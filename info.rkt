5c8da09180bbf49967c43b114f0efd609464707a
Rajan <walrajan@gmail.com>
2017-05-13 15:09:29 -0400

    fixing collection name and cleaning up

--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
 #lang info
-(define collection "test")
+
+(define collection "sham")
 (define deps '("base"
                "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/test.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(rajan))
+(define pkg-desc "compiler framework on top of llvm")
+(define version "0.1")
+
