b1f12709597d7b2932e1d2685096cf911bee0346
Rajan <walrajan@gmail.com>
2017-11-10 00:57:30 -0500

    adding dynext-lib as dependency

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "sham")
 (define deps '("base"
-               "rackunit-lib"))
+               "rackunit-lib"
+               "dynext-lib"))
 (define pkg-desc "compiler framework on top of llvm")
 (define version "0.1")
-
