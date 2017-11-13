0c05db357ed3e28442279bebe47b900220f36eed
Rajan <walrajan@gmail.com>
2017-11-12 21:28:10 -0500

    debug parameter in sham

--- private/utils.rkt
+++ private/utils.rkt
@@ -10,10 +10,10 @@
     (string->symbol (string-append (symbol->string sym)
                                    sep
                                    (number->string n)))))
+
 (define sham-diagnose (make-parameter #f))
+(define sham-debug (make-parameter #f))
 
 (define (symbol-append s1 s2)
   (string->symbol (string-append (symbol->string s1))
          (symbol->string s2)))
-(require "info-key.rkt")
-(provide (all-defined-out))
