64c8500894a2076deb802d6e3df5c8524d17acaf
Rajan <walrajan@gmail.com>
2018-07-31 08:55:43 -0400

    test template

---
+++ test/template.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require "../main.rkt")
+
+
+(module+ test
+  (require rackunit)
+
+  (define mod
+    (s$:dmodule
+     (empty-mod-env-info) 'test-module
+     (list
+      ...)))
+
+  (define mod-env (compile-module mod))
+  (jit-dump-module mod-env)
+  (basic-optimize-module mod-env #:opt-level 3)
+  (jit-dump-module mod-env)
+  (initialize-jit! mod-env)
+  (define ... (jit-get-function '... mod-env))
+  (check-eq? ... ...))
