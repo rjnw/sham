18df149dd0d8198e112a762a937db1b36e6b0c9b
Rajan <walrajan@gmail.com>
2018-07-31 08:55:58 -0400

    simple function tests

---
+++ test/functions.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(require "../main.rkt")
+
+
+(module+ test
+  (require rackunit)
+
+  (define mod
+    (s$:dmodule
+     (empty-mod-env-info) 'test-mod
+     (list
+      (s$:dfunction (void) 'const42 '() '() s$:i32
+                    (s$:ret (s$:ui32 42)))
+      (s$:dfunction (void) 'identity (list 'v) (list s$:i32) s$:i32
+                    (s$:ret (s$:v 'v)))
+      (s$:dfunction (void) 'even? (list 'x) (list s$:i32) s$:i32
+                    (s$:if (s$:app s$:icmp-eq (list (s$:app s$:urem (list (s$:v 'x) (s$:ui32 2)))
+                                                    (s$:ui32 0)))
+                           (s$:ret (s$:ui32 1))
+                           (s$:ret (s$:ui32 0)))))))
+
+  (define mod-env (compile-module mod))
+  (jit-dump-module mod-env)
+  (basic-optimize-module mod-env #:opt-level 3)
+  (jit-dump-module mod-env)
+  (initialize-jit! mod-env)
+
+  (define const42 (jit-get-function 'const42 mod-env))
+  (check-eq? (const42) 42)
+
+  (define id (jit-get-function 'identity mod-env))
+  (check-eq? (id 42) 42)
+
+  (define even-huh (jit-get-function 'even? mod-env))
+  (check-eq? (even-huh 42) 1))
