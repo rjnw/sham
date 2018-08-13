d8e11bf3a161632f0cc2c9281bb3e3edb25890db
Rajan <walrajan@gmail.com>
2018-08-13 07:14:31 -0400

    more recursion test

--- test/recursion.rkt
+++ test/recursion.rkt
@@ -23,11 +23,25 @@
                                     (s$:ui1 0))
                            (s$:ret (s$:ui1 0))
                            (s$:ret (s$:app^ (s$:rs 'meven?)
-                                            (s$:app^ s$:sub (s$:v 'x) (s$:ui32 1)))))))))
+                                            (s$:app^ s$:sub (s$:v 'x) (s$:ui32 1))))))
+
+      (s$:dfunction (void) 'factr '(x) (list s$:i32) s$:i32
+                    (s$:se (s$:let '(result i) (list s$:i32 s$:i32) (list (s$:ui32 1) (s$:ui32 1))
+                                   (s$:block^
+                                    (s$:while
+                                     (s$:app^ s$:icmp-ule (s$:v 'i) (s$:v 'x))
+                                     (s$:block^
+                                      (s$:set! (s$:v 'result)
+                                               (s$:app^ s$:mul-nuw (s$:v 'result) (s$:v 'i)))
+                                      (s$:set! (s$:v 'i)
+                                               (s$:app^ s$:add-nuw (s$:v 'i) (s$:ui32 1)))))
+                                    (s$:return (s$:v 'result)))
+                                   (s$:evoid)))))))
 
   (define mod-env (compile-module mod))
   (jit-dump-module mod-env)
-  (basic-optimize-module mod-env #:opt-level 3)
+  (optimize-module mod-env #:opt-level 3 #:size-level 1
+                   )
   (jit-dump-module mod-env)
   (initialize-jit! mod-env)
 
@@ -35,4 +49,7 @@
   (check-eq? (meven? 42) 1)
 
   (define modd? (jit-get-function 'modd? mod-env))
-  (check-eq? (modd? 42) 0))
+  (check-eq? (modd? 42) 0)
+
+  (define factr (jit-get-function 'factr mod-env))
+  (check-eq? (factr 5) 120))
