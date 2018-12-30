236758f4b6cdfa56e5489346cf4c6d6966b7a37e
rajan <wal.rajan@gmail.com>
2018-12-30 12:28:54 -0500

    merge-sham-module

--- private/jit-utils.rkt
+++ private/jit-utils.rkt
@@ -9,8 +9,7 @@
  "init.rkt"
  "parameters.rkt"
  "dump.rkt"
- ffi/unsafe
- )
+ ffi/unsafe)
 
 (provide (all-defined-out))
 (define (rptr->llvmptr rptr)
@@ -43,17 +42,35 @@
     [else (module-initialize-mcjit! cm)])
   (set-hmodule-cmod! hm cm))
 
-(define (get-module-func hm fid)
+(define (merge-sham-module into from)
+  (define (combine-info inf1 inf2)
+    (pretty-print inf1) (pretty-print inf2)
+    (printf "TODO: combine infos")
+    inf2)
+  (define (combine-hinfo inf1 inf2) inf1) ;TODO
+  (match-define (hmodule into-id into-func-map into-info into-hinfo into-cmod) into)
+  (match-define (hmodule from-id from-func-map from-info from-hinfo from-cmod) from)
+  (when (or into-cmod from-cmod)
+    (error "one or both sham modules already compiled before merging" into-id from-id))
+
+  (define new-func-map (make-hash (append (hash->list into-func-map) (hash->list from-func-map))))
+  (hmodule into-id
+           new-func-map
+           (combine-info into-info from-info)
+           (combine-hinfo into-hinfo into-hinfo)
+           #f))
+
+(define (sham-module-lookup-function hm fid)
   (if (hmodule-cmod hm)
       (jit-get-function fid (hmodule-cmod hm))
       (error "module not compiled.")))
 
 (define (sham-app hfunc . args)
   (match-define (hfunction id sargs argt rett bb finfo sm) hfunc)
-  (apply (get-module-func sm id) args))
+  (apply (sham-module-lookup-function sm id) args))
 (define (get-sham-function hfunc)
   (match-define (hfunction id sargs argt rett bb finfo sm) hfunc)
-  (get-module-func sm id))
+  (sham-module-lookup-function sm id))
 
 (define (sham-dump-llvm hm)
   (match-define (hmodule id func-map info hinfo cmod) hm)
