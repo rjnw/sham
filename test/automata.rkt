51e3ddaa36d91eef6dc301cd75505411a61971d6
rajan <wal.rajan@gmail.com>
2019-02-11 22:51:11 -0500

    more automata

--- test/automata.rkt
+++ test/automata.rkt
@@ -1,52 +1,78 @@
 #lang racket
 
 (require rcf/ast
-         (for-syntax syntax/parse racket/pretty))
+         sham
+         sham/ast-utils
+         sham/jit-utils
+         (for-syntax syntax/parse racket/pretty racket/set))
 
-;; (define-ast machine
-;;   #:custom-write #t
-;;   (def
-;;     [dfa (start
-;;           (end ...)
-;;           [state ([evt next-state]
-;;                   ...)]
-;;           ...)]))
+(define fsa-module
+  (create-empty-sham-module "module"))
+(current-sham-module fsa-module)
 
-(define-syntax (dfa stx)
+(define-syntax (define-fsa stx)
   (syntax-parse stx
-    [(_ start (end ...) [state ([evt next-state] ...)] ...)
+    [(_ name start (end ...) [state ([evt next-state] ...)] ...)
+     (define state-id-map
+       (make-hash (for/list ([s (syntax->list #`(state ...))]
+                             [i (in-range (length (syntax->list #`(state ...))))])
+                    (cons (syntax->datum s) (add1 i)))))
+     (define end-states (list->set (syntax->datum #`(end ...))))
+     (pretty-display state-id-map)
+     (define funcs
+       #`(begin
+           (define-sham-function (state (inp : i64*) (pos : i64) (len : i64) : i64)
+             (if^ (icmp-ule pos len)
+                  (switch^ (load (gep^ inp pos))
+                           [(ui64 evt) (return (next-state inp (add pos (ui64 1)) len))] ...
+                           (return (ui64 0)))
+                  (return (ui64 1)))) ...))
+     (pretty-display (syntax->datum funcs))
      (define fs (for/list  ([s (syntax->list #`(state ...))]
                             [sevt (syntax->list #`((evt ...) ...))]
                             [snxs (syntax->list #`((next-state ...) ...))])
-                  (pretty-display s)
-                  #`(define-sham-function #,s (input : i64) : )
-                  (pretty-display sevt)
-                  (pretty-display snxs)
-                  42))
-     #'42]))
+                  (define f
+                    #`(define-sham-function (#,s (inp : i64*) (pos : i64) (len : i64) : i64)
+                        (if^ (icmp-ule pos len)
+                             (switch^ (load (gep^ inp pos))
+                                      #,@(map (λ (s ns) #`[(ui64 #,s)
+                                                           (return (#,ns inp (add pos (ui64 1))))])
+                                              (syntax->list sevt) (syntax->list snxs))
+                                      (return (ui64 0)))
+                             #,(if (set-member? end-states (syntax->datum s))
+                                   #`(return (ui64 1))
+                                   #`(return (ui64 0))))))
+                  f))
+     (pretty-display (map syntax->datum fs))
+     #`(begin #,@fs)]))
 
-(define M^
-  (dfa s1 (s1)
-       [s1 ([0 s2]
-            [2 s1])]
-       [s2 ([0 s1]
-            [2 s2]
-            [4 s2])]))
+(define-fsa M
+  s1 (s1)
+  [s1 ([0 s2]
+       [2 s1])]
+  [s2 ([0 s1]
+       [2 s2]
+       [4 s2])])
 
-(define (s1 e)
-  (match e
-    [0 s2]
-    [2 s1]))
-(define (s2 e)
-  (match e
-    [0 s1]
-    [2 s2]
-    [4 s2]))
-(define M (cons s1 (list s1)))
-(define (machine-accepts? M ls)
-  (define final
-    (for/fold ([current (car M)])
-              ([l ls])
-      (current l)))
-  (not (false? (member final (cdr M)))))
-(machine-accepts? M (list 2 0 4 0 2))
+;; (parameterize ([compile-options (list 'pretty 'dump 'verify)])
+;;   (compile-sham-module!
+;;    fsa-module
+;;    #:opt-level 3))
+
+;; (define (s1 e)
+;;   (match e
+;;     [0 s2]
+;;     [2 s1]))
+;; (define (s2 e)
+;;   (match e
+;;     [0 s1]
+;;     [2 s2]
+;;     [4 s2]))
+;; (define M (cons s1 (list s1)))
+;; (define (machine-accepts? M ls)
+;;   (define final
+;;     (for/fold ([current (car M)])
+;;               ([l ls])
+;;       (current l)))
+;;   (not (false? (member final (cdr M)))))
+;; (machine-accepts? M (list 2 0 4 0 2))
