875ce6e75e6597f2acf6d49c36f1a0f3fcf96b29
rajan <wal.rajan@gmail.com>
2019-02-25 14:14:50 -0500

    automata changes

--- test/automata.rkt
+++ test/automata.rkt
@@ -55,6 +55,7 @@
   [s2 ([0 s1]
        [1 s2]
        [2 s2])])
+
 (define-fsa M^
   init (end)
   [init ([0 more])]
@@ -63,16 +64,16 @@
          [3 end])]
   [end ()])
 
-(parameterize ([compile-options (list 'pretty 'dump 'verify)])
+(parameterize ([compile-options (list 'dump 'verify)])
   (compile-sham-module!
    fsa-module
    #:opt-level 3))
 
 (require ffi/unsafe
          racket/fixnum)
-(define input (for/vector ([i 10000000]) (random 3)))
-(define sham-input (time (vector->cpointer input)))
-
+(define input (for/vector ([i 1000000]) (random 3)))
+(define sham-input (vector->cpointer input))
+(define il (vector-length input))
 (let ()
   (define-racket-fsa M
     s1 (s1)
@@ -83,5 +84,23 @@
          [1 s2]
          [2 s2])])
   (time (M input 0 (vector-length input))))
+(time (sham-app M sham-input 0 il))
+(time (sham-app M sham-input 0 il))
 
-(time (sham-app M sham-input 0 (vector-length input)))
+(define cinput (cons 0 (append (for/list ([i 10000000]) (add1 (random 2)))
+                               (list 3))))
+(define cv (list->vector cinput))
+(define clen (vector-length cv))
+(define cvs (vector->cpointer cv))
+
+(let ()
+  (define-racket-fsa M^
+    init (end)
+    [init ([0 more])]
+    [more ([1 more]
+           [2 more]
+           [3 end])]
+    [end ()])
+  (time (M^ cv 0 clen)))
+(time (sham-app M^ cvs 0 clen))
+(time (sham-app M^ cvs 0 clen))
