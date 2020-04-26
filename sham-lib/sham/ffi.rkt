d6a30a9abc253c2efee61274f291646bd49122c6
rajan <wal.rajan@gmail.com>
2020-04-25 20:49:05 -0400

    code for getting ffi uintptr

---
+++ sham-lib/sham/ffi.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(require ffi/unsafe
+         (prefix-in foreign: '#%foreign))
+
+(define-cstruct _scheme_object  ([typetag _short]   [key _short]))
+(define-cstruct _ffi_obj_struct
+  ([iso _scheme_object]
+   [obj _uintptr]
+   [name _string]
+   [flib _pointer]))
+
+(define (get-ffi-pointer ffi-lib fname)
+  (define fptr-obj (foreign:ffi-obj (string->bytes/locale fname) ffi-lib))
+  (define fname-ffi-obj (cast fptr-obj _scheme _ffi_obj_struct-pointer))
+  (ffi_obj_struct-obj fname-ffi-obj))
