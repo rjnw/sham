8329572f4bcc29091de24ec80ba393801242e2e4
rajan <wal.rajan@gmail.com>
2020-05-04 16:36:16 -0400

    using stmts instead of expressions for sham automata

--- sham-test/sham/test/automata/automata.rkt
+++ sham-test/sham/test/automata/automata.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require sham
+         sham/llvm/ir/callconv
          (prefix-in rkt- sham/rkt))
 
 (require (for-syntax syntax/parse))
@@ -14,15 +15,28 @@
 
 (define-syntax (benchmark stx)
   (syntax-parse stx
-    [(_ mesg:expr (proc:id args:expr ...))
-     #`(let-values ([(result cpu-ms real-ms garbage-ms) (time-apply proc (list args ...))])
-         (when (print-time)
-           (printf "sham:benchmark:~a: cpu: ~a, real: ~a, gc: ~a\n" mesg cpu-ms real-ms garbage-ms))
-         (hash-set! bench-times mesg
-                    (cons (bench-time cpu-ms real-ms garbage-ms) (hash-ref bench-times mesg '())))
-         (apply values result))]))
+    [(_ repeat mesg:expr (proc:id args:expr ...))
+     #`(for/last ([r repeat])
+         (let-values ([(result cpu-ms real-ms garbage-ms) (time-apply proc (list args ...))])
+           (when (print-time)
+             (printf "sham:benchmark:~a: cpu: ~a, real: ~a, gc: ~a\n" mesg cpu-ms real-ms garbage-ms))
+           (hash-set! bench-times mesg
+                      (cons (bench-time cpu-ms real-ms garbage-ms) (hash-ref bench-times mesg '())))
+           (apply values result)))]))
 
-(define (racket-automata input input-length)
+(define (average-times)
+  (for/hash ([(k times) bench-times])
+    (define-values (cpu real garbage)
+      (for/fold [(cpu 0)
+                 (real 0)
+                 (garbage 0)]
+                [(t times)]
+        (match-define (bench-time c r g) t)
+        (values (+ cpu c) (+ real r) (+ garbage g))))
+    (define l (exact->inexact (length times)))
+    (values k (bench-time (/ cpu l) (/ real l) (/ garbage l)))))
+
+(define (racket-automata input input-length ir)
   (define-syntax (define-fsa stx)
     (syntax-parse stx
       [(_ name start (end ...) [state ([input next-state] ...)] ...)
@@ -38,14 +52,14 @@
                    [(input) (next-state inp (+ pos 1) len)] ...
                    [else false])
                  res)) ...)]))
-  (define-fsa M-simple
-    s1 (s1)
-    [s1 ([c s2]
-         [a s2]
-         [d s1])]
-    [s2 ([c s1]
-         [a s2]
-         [r s2])])
+  #;(define-fsa M-simple
+      s1 (s1)
+      [s1 ([c s2]
+           [a s2]
+           [d s1])]
+      [s2 ([c s1]
+           [a s2]
+           [r s2])])
   (define-fsa M-cadr
     init (end)
     [init ([c more])]
@@ -53,13 +67,15 @@
            [d more]
            [r end])]
     [end ()])
-  (values (benchmark (format "rkt-fsa-simple:~a" input-length)
+  #;(values (benchmark ir (format "rkt-fsa-simple:~a" input-length)
                      (M-simple input 0 input-length))
-          (benchmark (format "rkt-fsa-cadr:~a" input-length)
-                     (M-cadr input 0 input-length))))
+          (benchmark ir (format "rkt-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
+  (benchmark ir (format "rkt-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
 
-(define (sham-automata input input-length)
-  (define-current-sham-env fsa-module #:late-pass `[always-inliner])
+(define (sham-automata input input-length ir)
+  (define-current-sham-env default-module #:late-pass `[always-inliner])
   (define fmd (sham-function-metadata #:attributes `[always-inline]))
 
   (define-syntax (define-fsa stx)
@@ -69,16 +85,16 @@
        (map (lambda (e) (if (memq (syntax-e e) (syntax->datum #'(end ...))) #'rkt-true #'rkt-false))
             (syntax->list #'(state ...)))
        #'(begin
-           (define-sham-efunction (name (inp : rkt-sym*) (pos : i64) (len : i64) : rkt-bool)
-             (start inp pos len))
-           (define-sham-efunction #:md fmd (state (inp : rkt-sym*) (pos : i64) (len : i64) : rkt-bool)
-             (e-if^ rkt-bool (icmp-ult^ pos len)
-                    (rkt-e-sym-case rkt-bool (array-ref^ inp pos)
-                                    [(input) (next-state inp (add^ pos (ui64 1)) len)] ...
-                                    [else rkt-false])
-                    res)) ...)]))
+           (define-sham-function (name (inp : rkt-sym*) (pos : i64) (len : i64) : rkt-bool)
+             (return^ (start inp pos len)))
+           (define-sham-function #:md fmd (state (inp : rkt-sym*) (pos : i64) (len : i64) : rkt-bool)
+             (if^ (icmp-ult^ pos len)
+                  (switch^ (array-ref^ inp pos)
+                           [(rkt-sym input) (return^ (next-state inp (add^ pos (ui64 1)) len))] ...
+                           (return^ rkt-false))
+                  (return^ res))) ...)]))
 
-  (define-fsa M-simple
+  #;(define-fsa M-simple
     s1 (s1)
     [s1 ([c s2]
          [a s2]
@@ -95,14 +111,61 @@
            [r end])]
     [end ()])
 
-  (sham-jit-compile! fsa-module #:opt-level 3)
-  (values (benchmark (format "sham-fsa-simple:~a" input-length)
+  (sham-jit-compile! default-module #:opt-level 3)
+  #;(values (benchmark ir (format "sham-fsa-simple:~a" input-length)
                      (M-simple input 0 input-length))
-          (benchmark (format "sham-fsa-cadr:~a" input-length)
-                     (M-cadr input 0 input-length))))
+          (benchmark ir (format "sham-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
+  (benchmark ir (format "sham-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
 
-(define (basic-block-automata input input-length)
-  (define-current-sham-env fsa-module)
+(define (fastcc-automata input input-length ir)
+  (define-current-sham-env fastcc-module)
+  (define fmd (sham-function-metadata #:calling-convention fastcc-md))
+
+  (define-syntax (define-fsa stx)
+    (syntax-parse stx
+      [(_ name start (end ...) [state ([input next-state] ...)] ...)
+       #:with (res ...)
+       (map (lambda (e) (if (memq (syntax-e e) (syntax->datum #'(end ...))) #'rkt-true #'rkt-false))
+            (syntax->list #'(state ...)))
+       #'(begin
+           (define-sham-function (name (inp : rkt-sym*) (pos : i64) (len : i64) : rkt-bool)
+             (return^ (start inp pos len)))
+           (define-sham-function #:md fmd (state (inp : rkt-sym*) (pos : i64) (len : i64) : rkt-bool)
+             (if^ (icmp-ult^ pos len)
+                  (switch^ (array-ref^ inp pos)
+                           [(rkt-sym input) (return^ (next-state inp (add^ pos (ui64 1)) len))] ...
+                           (return^ rkt-false))
+                  (return^ res))) ...)]))
+
+  #;(define-fsa M-simple
+    s1 (s1)
+    [s1 ([c s2]
+         [a s2]
+         [d s1])]
+    [s2 ([c s1]
+         [a s2]
+         [r s2])])
+
+  (define-fsa M-cadr
+    init (end)
+    [init ([c more])]
+    [more ([a more]
+           [d more]
+           [r end])]
+    [end ()])
+
+  (sham-jit-compile! fastcc-module #:opt-level 3)
+  #;(values (benchmark ir (format "fastcc-fsa-simple:~a" input-length)
+                     (M-simple input 0 input-length))
+            (benchmark ir (format "fastcc-fsa-cadr:~a" input-length)
+                       (M-cadr input 0 input-length)))
+  (benchmark ir (format "fastcc-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
+
+(define (basic-block-automata input input-length ir)
+  (define-current-sham-env basic-block-module)
 
   (define-syntax (define-fsa stx)
     (syntax-parse stx
@@ -120,7 +183,7 @@
                                       [else (return^ rkt-false)])
                         (return^ res))) ...)]))
 
-  (define-fsa M-simple
+  #;(define-fsa M-simple
     s1 (s1)
     [s1 ([c s2]
          [a s2]
@@ -137,11 +200,13 @@
            [r end])]
     [end ()])
 
-  (sham-jit-compile! fsa-module #:opt-level 3)
-  (values (benchmark (format "bb-fsa-simple:~a" input-length)
+  (sham-jit-compile! basic-block-module #:opt-level 3)
+  #;(values (benchmark ir (format "bb-fsa-simple:~a" input-length)
                      (M-simple input 0 input-length))
-          (benchmark (format "bb-fsa-cadr:~a" input-length)
-                     (M-cadr input 0 input-length))))
+          (benchmark ir (format "bb-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
+  (benchmark ir (format "bb-fsa-cadr:~a" input-length)
+                     (M-cadr input 0 input-length)))
 
 
 (module+ test
@@ -149,27 +214,25 @@
            ffi/unsafe
            racket/random)
 
-  (define (run-test! (repeat-count 1) (input-length 100000))
+  (define (run-test! (repeat-count 1) (internal-repeat 10000) (input-length 100000))
     (define input (build-vector input-length (λ (i)
                                                (cond [(zero? i) 'c]
                                                      [(equal? i (sub1 input-length)) 'r]
                                                      [else (random-ref `(a d))]))))
-    (define sham-input (benchmark (format "sham-rkt-vector-cast:~a" input-length)
+    (define sham-input (benchmark 1 (format "sham-rkt-vector-cast:~a" input-length)
                                   (rkt-vector rkt-sym input)))
-    (for/list ([i repeat-count])
+    (printf "done-making-input\n")
+    (for ([i repeat-count])
       (define info (format ":test ~a" i))
-      (define-values (r-simple r-cadr) (racket-automata input input-length))
-      (define-values (s-simple s-cadr) (sham-automata sham-input input-length))
-      (define-values (b-simple b-cadr) (basic-block-automata sham-input input-length))
-      (test-equal? (format "automata~a:~a sham:simple" info input-length) s-simple r-simple)
-      (test-equal? (format "automata~a:~a sham:cadr" info input-length) s-cadr r-cadr)
-      (test-equal? (format "automata~a:~a bb:simple" info input-length) b-simple r-simple)
-      (test-equal? (format "automata~a:~a bb:cadr" info input-length) b-cadr r-cadr)
-      `(,i (rkt ,r-simple ,r-cadr)
-           (sham ,s-simple ,s-cadr)
-           (basic-block ,b-simple ,b-cadr))))
+      (define r  (racket-automata input input-length internal-repeat))
+      (define s (sham-automata sham-input input-length internal-repeat))
+      (define b (basic-block-automata sham-input input-length internal-repeat))
+      (define f (fastcc-automata sham-input input-length internal-repeat))
+      (test-equal? (format "automata~a:~a sham:cadr" info input-length) s r)
+      (test-equal? (format "automata~a:~a bb:cadr" info input-length) b r)
+      (test-equal? (format "automata~a:~a fastcc:cadr" info input-length) f r)))
 
-  (parameterize ([sham-compile-options `(;; dump-sham dump-llvm dump-llvm-ir-after-opt verify-with-error
-                                         )])
-    (pretty-print (run-test! 4)))
-  (pretty-print bench-times))
+  (parameterize ([sham-compile-options `(;; dump-sham dump-llvm
+                                         dump-llvm-ir-after-opt verify-with-error)])
+    (run-test! 1 10 1000))
+  (pretty-print (average-times)))
