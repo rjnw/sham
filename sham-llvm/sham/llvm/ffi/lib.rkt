de0b4e732be95400b19f21a2032f7e00ae2e0b8f
Andre Kuhlenschmidt <akuhlens@fb.com>
2021-10-21 06:22:10 -0700

    make lib script more flexible

--- sham-llvm/sham/llvm/ffi/lib.rkt
+++ sham-llvm/sham/llvm/ffi/lib.rkt
@@ -9,14 +9,25 @@
 
 (define llvm-version-string (llvm-config "--version"))
 (define llvm-lib-path (llvm-config "--libdir"))
+(define llvm-major-version-string
+  (let ([matches (regexp-match #px"^([0-9]+)\\." llvm-version-string)])
+    (if matches
+        (list-ref matches 1)
+        #f)))
 
 (define llvm-lib
-  (ffi-lib
-   (build-path "libLLVM")
-   (list (llvm-config "--version") #f)
-   #:get-lib-dirs
-   (case (system-type 'os)
-     [(macosx unix)
-      (lambda () (list (llvm-config "--libdir")))]
-     [(windows) get-lib-search-dirs])
-   #:global? #t))
+  (cond
+    [(getenv "LLVM_LIB_PATH") => (λ (path) (ffi-lib (build-path path)))]
+    [else
+     (define files-ls (string-split (llvm-config "--libfiles")))
+     (cond
+       [(= (length files-ls) 1) (ffi-lib (build-path (car files-ls)))]
+       [else
+        (ffi-lib
+         (build-path "libLLVM")
+         (list llvm-major-version-string #f)
+         #:get-lib-dirs
+         (case (system-type 'os)
+           [(macosx unix)
+            (lambda () (list llvm-lib-path))]
+           [(windows) get-lib-search-dirs]))])]))
