d3e58f2a8484f0ca5815a1e610bb41aa665f396c
Rajan Walia <wal.rajan@gmail.com>
2021-12-04 09:54:42 -0800

    more tests

--- sham-examples/sham/cryptol/tests/sequence.rkt
+++ sham-examples/sham/cryptol/tests/sequence.rkt
@@ -2,6 +2,12 @@
 
 #:compile-with sham
 
+;; (test str2 (== "ab" [#x61 #x62]))
+;; (test str11 (== ["a"] ["a"]))
+;; (test str3 (== "abc" [#x61 #x62 #x63]))
+;; (test ss1 (== [["a"] ["b"] ["c"]] [["a"] ["b"] ["c"]]))
+;; (test en1 (== [(1 : [64]) .. 4] [1 2 3 4]))
+
 (def
   [const1 : {a} a -> [1 a]]
   [const1 v = [v]])
@@ -13,9 +19,9 @@
 (test c1-bit (== (const1 (: 42 [64])) [42]))
 (test c1-bbit (== (const1 (: [42] [1 [64]])) [[42]]))
 
-(def
-  [proj1 : {a} [1 a] -> a]
-  [proj1 [v] = v])
+;; (def
+;;   [proj1 : {a} [1 a] -> a]
+;;   [proj1 [v] = v])
 
-(test p1-bit (== (proj1 (: [42] [1 [64]])) 42))
-(test p1-bbit (== (proj1 [(: [42] [1 [64]])]) (: [42] [1 [64]])))
+;; (test p1-bit (== (proj1 (: [42] [1 [64]])) 42))
+;; (test p1-bbit (== (proj1 [(: [42] [1 [64]])]) (: [42] [1 [64]])))
